c9581e9ec1d3741163cacf802271111f706c4f9c
Mark Feaver <mark.feaver@gmail.com>
2014-05-18 17:21:54 +1200

    Increased tentacle damage to make them more useful

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -53,7 +53,7 @@ public class ShipController : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		// If the ship is hit by a tentacle, reduce health
 		if (other.gameObject.tag == "TentaclePalm"){
-			currentHealth -= 25.0f;
+			currentHealth -= 40.0f;
 			rigidbody2D.AddForce(other.attachedRigidbody.velocity * 20.0f);
 			AudioSource.PlayClipAtPoint(damageSound, transform.position);
 			gameController.AddScore(tentacleScore);
