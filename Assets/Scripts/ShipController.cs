613ec8afe947e6dcfe2f967eb238024decd1b3a1
Mark Feaver <mark.feaver@gmail.com>
2014-05-24 10:57:00 +1200

    Fixed damage to ships after death bug

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -51,25 +51,29 @@ public class ShipController : MonoBehaviour {
 //	}
 	
 	void OnTriggerEnter2D(Collider2D other){
-		// If the ship is hit by a tentacle, reduce health
-		if (other.gameObject.tag == "TentaclePalm"){
-			currentHealth -= 40.0f;
-			rigidbody2D.AddForce(other.attachedRigidbody.velocity * 20.0f);
-			AudioSource.PlayClipAtPoint(damageSound, transform.position);
-			gameController.AddScore(tentacleScore);
-			UpdateHealthLabel();
-		}
-		else if (other.gameObject.tag == "PlayerBody"){
-			PlayerController player = other.GetComponent<PlayerController>();
-			if (player.isBoosting){
-				currentHealth -= 50.0f;
-				rigidbody2D.AddForce(other.attachedRigidbody.velocity * 40.0f);
-				AudioSource.PlayClipAtPoint(bigDamageSound, transform.position);
-				gameController.AddScore(bodyHitScore);
+		// If the player is dead, don't increase the score, etc
+		if (!gameController.isPlayerDead()){
+			// If the ship is hit by a tentacle, reduce health
+			if (other.gameObject.tag == "TentaclePalm"){
+				currentHealth -= 40.0f;
+				rigidbody2D.AddForce(other.attachedRigidbody.velocity * 20.0f);
+				AudioSource.PlayClipAtPoint(damageSound, transform.position);
+				gameController.AddScore(tentacleScore);
 				UpdateHealthLabel();
 			}
-		}	
-		else if (other.gameObject.tag == "Destroyer"){
+			else if (other.gameObject.tag == "PlayerBody"){
+				PlayerController player = other.GetComponent<PlayerController>();
+				if (player.isBoosting){
+					currentHealth -= 50.0f;
+					rigidbody2D.AddForce(other.attachedRigidbody.velocity * 40.0f);
+					AudioSource.PlayClipAtPoint(bigDamageSound, transform.position);
+					gameController.AddScore(bodyHitScore);
+					UpdateHealthLabel();
+				}
+			}	
+		}
+		// If ship goes out of bounds, destroy it
+		if (other.gameObject.tag == "Destroyer"){
 			Destroy(gameObject);
 		}
 	}
