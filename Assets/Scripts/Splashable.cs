14796880b3e9fae7f541f79310a462940b0d7537
Mark Feaver <mark.feaver@gmail.com>
2014-06-10 18:35:38 +1200

    Added a splash sound when player hits the water

---
+++ Assets/Scripts/Splashable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splashable : MonoBehaviour {
+
+	// The sound to play when the player hits the water
+	public AudioClip playerSplash;
+	public float yPositionWater = 0.0f;
+	
+	// Used to calculate if the character is travelling downwards (for water splashes, etc)
+	private bool shouldPlaySample = false;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Figure out if we need to create a splash or not
+		if (transform.position.y < yPositionWater){
+			// If we're travelling down the screen, create a splash
+			if (shouldPlaySample){
+				AudioSource.PlayClipAtPoint(playerSplash, transform.position);
+				shouldPlaySample = false;
+			}
+		}
+		else {
+			shouldPlaySample = true;
+		}
+	}
+}
